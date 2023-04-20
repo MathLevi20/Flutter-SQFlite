ea8b4cb63f0dea60e272af6a170b4553bb45b4fc
Levi <matheuslevi2012@gmail.com>
2023-04-20 15:40:39 -0300

    deploy bitrise new test

--- lib/controller/Myapp.dart
+++ lib/controller/Myapp.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Perfil'),
+      home: MyHomePage(title: 'Perfil'),
     );
   }
 }
